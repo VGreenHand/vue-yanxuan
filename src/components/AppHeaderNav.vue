c37ea630e89b0b98045558126b38ec5b53769589
VGreenHand <914232759@qq.com>
2025-04-01 12:02:25 +0800

    删除旧的国家旗帜和地图精灵图像，新增应用头部导航组件，更新头部样式和搜索功能

---
+++ src/components/AppHeaderNav.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <ul class="app-header-nav">
+      <li>
+        <router-link>首页</router-link>
+        <!-- 悬停弹窗 -->
+        <div class="hover-box">
+
+        </div>
+
+      </li>
+      <li><a href="#">首页</a></li>
+      <li><a href="#">首页</a></li>
+      <li><a href="#">首页</a></li>
+      <li><a href="#">首页</a></li>
+      <li><a href="#">首页</a></li>
+      <li><a href="#">首页</a></li>
+      <li><a href="#">首页</a></li>
+      <li><a href="#">首页</a></li>
+      <li><a href="#">首页</a></li>
+      <li><a href="#">首页</a></li>
+      <li><a href="#">首页</a></li>
+      <li><a href="#">首页</a></li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="less" scoped>
+.app-header-nav {
+  li {
+    float: left;
+    padding: 0 10px;
+    font-weight: bold;
+    height: 40px;
+    line-height: 40px;
+    // 默认选择首页高亮
+    .router-link-exact-active {
+      color: @xtxColor;
+      border-bottom: 2px solid @xtxColor;
+    }
+    a {
+      font-size: 15px;
+      padding-bottom: 9px;
+      &:hover {
+        color: @xtxColor;
+        border-bottom: 2px solid @xtxColor;
+      }
+    }
+    // a:hover{
+    //         color: @xtxColor;
+    //         border-bottom: 2px solid @xtxColor;
+    // }
+  }
+}
+</style>
--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,58 +1,134 @@
 <template>
-    <div class="my-header w">
-        <!-- 上区域 -->
-        <div class="header-top">
-            <!-- 左侧区域 -->
-            <h1 class="logo">
-                <router-link to="/"></router-link>
-            </h1>
-            <!-- 中间内容区域 -->
-            <div class="search">
-                <div class="center-search">
-                    <input type="text" placeholder="搜索商品,共223个好物" class="search-input" />
-                    <button class="search-btn">搜索</button>
-                </div>
-            </div>
-            <!-- 右侧区域 -->
-            <div class="cart">
-
-            </div>
-
+  <div class="my-header w">
+    <!-- 上区域 -->
+    <div class="header-top">
+      <!-- 左侧区域 -->
+      <h1 class="logo">
+        <router-link to="/"></router-link>
+      </h1>
+      <!-- 中间内容区域 -->
+      <div class="search">
+        <div class="center-search">
+          <input type="text" placeholder="搜索商品,共223个好物" class="search-input" />
+          <button class="search-btn">搜索</button>
         </div>
+        <!-- 搜索区域下的小导航 -->
+        <ul class="search-nav">
+          <li><a href="#">美食</a></li>
+          <li><a href="#">餐厨</a></li>
+          <li><a href="#">艺术</a></li>
+          <li><a href="#">电器</a></li>
+          <li><a href="#">居家</a></li>
+          <li><a href="#">洗护</a></li>
+          <li><a href="#">育婴</a></li>
+        </ul>
+      </div>
+      <!-- 右侧区域 -->
+      <div class="cart">
+        <a href="#">
+            <i class="iconfont icon-gouwuchekong"></i>
+            购物车
+            <em>2</em>
+        </a>
+      </div>
     </div>
+    <!-- 下区域 二级导航-->
+     <AppHeaderNav />
+  </div>
 </template>
 <script>
+import AppHeaderNav from './AppHeaderNav.vue';
 export default {
-
-}
+    components: {
+        AppHeaderNav
+    }
+};
 </script>
 <style lang="less" scoped>
-.my-header{
-    height: 120px;
-    padding: 25px 0 0 0;
-    background-color: #fff;
-    .header-top{
-        display: flex;
-        .logo{
-            width: 230px;
-            height: 60px;
-            a{
-                display: inline-block;
-                width: 100%;
-                height: 100%;
-                background: url('../assets/images/logo.png') no-repeat 0 0px;
-                background-size: 100% 100%;
-            }
-
-        }
-        .search{
-            flex : 1;
-
-        }
-        .cart{
-
-        }
+.my-header {
+  height: 120px;
+  padding: 25px 0 0 0;
+  background-color: #fff;
+  .header-top {
+    display: flex;
+    .logo {
+      width: 330px;
+      height: 60px;
+      a {
+        display: inline-block;
+        width: 100%;
+        height: 60px;
+        background: url("../assets/images/sprite.png") no-repeat 0 -75px;
+        background-size: 330px 600px;
+      }
     }
+    .search {
+      flex: 1;
+      .center-search {
+        width: 540px;
+        height: 35px;
+        input {
+          width: 440px;
+          height: 35px;
+          border-radius: 20px 0 0 20px;
+          padding-left: 15px;
+          border: 1px solid @xtxColor;
+          box-sizing: border-box;
+        }
+        .search-btn {
+          float: right;
+          height: 35px;
+          width: 100px;
+          border: 0;
+          border-radius: 0 20px 20px 0;
+          background-color: @xtxColor;
+          padding-right: 15px;
+          border: 1px solid @xtxColor;
+          color: #fff;
+          font-size: 16px;
+          cursor: pointer;
+        }
+      }
+      .search-nav {
+        width: 540px;
+        li {
+          float: left;
+          height: 32px;
+          padding: 8px 0;
+          a {
+            &:hover {
+              color: @xtxColor;
+            }
+            padding: 0 10px;
+            border-right: 1px solid #ccc;
+            font-size: 12px;
+            // color: #ccc;
+          }
+        }
+        li:last-child a {
+          border-right: 0;
+        }
+      }
+    }
+  }
+  .cart {
+    width: 150px;
+    height: 35px;
+    line-height: 35px;
+    border-radius: 20px;
+    border: 1px solid @xtxColor;
+    font-size: 16px;
+    text-align: center;
+    em{
+        display: inline-block;
+        width: 18px;
+        height: 18px;
+        line-height: 18px;
+        background-color: #f00;
+        color: #fff;
+        border-radius: 50%;
+        margin-left: 5px;
+    }
+  }
 }
-
 </style>
